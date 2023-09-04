036f146eb02c575f8c7c4609d2abe1090257e475
Ramez <40645907+ImRamez@users.noreply.github.com>
2023-09-03 20:48:54 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const hourEl = document.getElementById("hour");
+
+const minuteEl = document.getElementById("minutes");
+
+const secondEl = document.getElementById("seconds");
+
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h >= 12) {
+    ampm = "PM";
+  }
+  if (h > 12) {
+    h = h - 12;
+  }
+
+  // if H is less than 10,
+  // if this happens
+  // just write zero _ h, otherwise  use h:
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl, (innerText = ampm);
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
